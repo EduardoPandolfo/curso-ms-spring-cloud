b7e9c929ceb32af1b9b7691d383a847adf886f27
EduardoPandolfo <ekpandolfo@gmail.com>
2023-08-09 11:03:22 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/eduardokp/hrworker/entities/Worker.java
@@ -0,0 +1,38 @@
+package com.eduardokp.hrworker.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_worker")
+public class Worker implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double dailyIncome;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Worker worker = (Worker) o;
+        return Objects.equals(id, worker.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
