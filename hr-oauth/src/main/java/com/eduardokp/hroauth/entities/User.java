49f3c48b5c810427c0e05daabd2a4fb42bd36dc6
EduardoPandolfo <ekpandolfo@gmail.com>
2023-08-14 18:23:19 -0300

    Login, Jwt token

--- hr-oauth/src/main/java/com/eduardokp/hroauth/entities/User.java
+++ hr-oauth/src/main/java/com/eduardokp/hroauth/entities/User.java
@@ -5,18 +5,23 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
-public class User implements Serializable {
-
+public class User implements UserDetails, Serializable {
+    private static final long serialVersionUID = 1L;
     private Long id;
     private String name;
     private String email;
@@ -34,5 +39,37 @@ public class User implements Serializable {
     @Override
     public int hashCode() {
         return Objects.hash(id);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(r -> new SimpleGrantedAuthority(r.getRoleName()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
