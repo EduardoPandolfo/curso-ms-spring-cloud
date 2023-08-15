6b41d223daed7dba20199767a93895ce2f0b905c
EduardoPandolfo <ekpandolfo@gmail.com>
2023-08-15 15:30:49 -0300

    Remoção de configuração de teste

--- hr-worker/src/main/java/com/eduardokp/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/eduardokp/hrworker/resources/WorkerResource.java
@@ -25,8 +25,8 @@ public class WorkerResource {
 
     private static final Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
-    @Value("${test.config}")
-    private String testConfig;
+//    @Value("${test.config}")
+//    private String testConfig;
 
     @Autowired
     private Environment env;
@@ -42,8 +42,7 @@ public class WorkerResource {
 
     @GetMapping(value = "/configs")
     public ResponseEntity<Void> getConfigs() {
-        logger.info("CONFIG -> " + testConfig);
-
+        //logger.info("CONFIG -> " + testConfig);
         return ResponseEntity.noContent().build();
     }
 
